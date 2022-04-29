79c4d11175bd39c931db9b4d5aa62fb3a74f460d
Mario Eduardo Guerrero Cervantes <marioe997@gmail.com>
2022-04-29 00:12:46 -0500

    Se agergan comentarios a clases

--- curso plsql principiantes/sentencia select/Renombrando columnas en queries.sql
+++ curso plsql principiantes/sentencia select/Renombrando columnas en queries.sql
@@ -1,6 +1,8 @@
+/* Si en el ass no agregamos comillas nos convertira el testo a uppercase */
 SELECT LAST_NAME AS name, COMMISSION_PCT comm
 FROM EMPLOYEES;
 
+/* Si el texto lo dejamos sin comillas nos respetara el texto tal como lo ingresamos */
 SELECT LAST_NAME AS "Nombre", salary * 12 "Salario anual", salary "Salario"
 FROM EMPLOYEES;
 
